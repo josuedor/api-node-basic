a9aa513ab00cce3cce051be206e081207b25b3fc
Josh <josuedor@gmail.com>
2018-01-28 23:11:02 -0500

    Generando documentacion

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const config = { 
-    hostname: '127.0.0.1',
-    port: 3000,
-    db: {
-        url: 'mongodb://localhost/database'
-    }
-};
-
-module.exports = config;
--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const config = require('./config/config')
-const server = require('./server/server')
-
-server.listen(config.port, config.hostname, () => {
-  console.log(`Server running at http://${config.hostname}:${config.port}/`);
-});
